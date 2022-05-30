3e26db04de22a36b639deb085de4d33dd645d4b5
Eric Cacciatori Bellini <eric.cacciatori@hotmail.com>
2022-05-30 15:55:47 -0300

    mini projeto concluido

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Bug Resolvido");
+﻿
+void funcaoFizzBuzz(int c)
+{
+    for(int n = 0; n <= c; n++)
+    {
+        string resultado = "";
+        if (n % 3 == 0) resultado = "Fizz";
+        if (n % 5 == 0) resultado = resultado + "Buzz";
+        if (resultado.Length == 0) resultado = n.ToString();
+        Console.WriteLine(resultado);
+    }
+}
+
+void main() {
+    funcaoFizzBuzz(543);
+}
+
+main();
